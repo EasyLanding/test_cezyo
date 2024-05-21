a445f2d3eb7d15782cfcfc010a91e110345cb855
Georgii Antonevich <easylanding.online@gmail.com>
2024-05-21 10:52:57 +0300

    update main and add Provider store and route provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
+import router from './routes/route-settings.tsx'
+import { Provider } from 'react-redux'
+import store from '@store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div style={{ display: 'flex', width: '100%' }}>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </div>
 )
